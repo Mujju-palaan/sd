f08a8f2cb43a1a611a8b751755ac2fe41d542d3e
Mujju-palaan <mdmujahid97@gmail.com>
2023-09-22 02:06:55 +0530

    deploy

---
+++ components/AllOtherCards/FlipperCard/FlipperCard.js
@@ -0,0 +1,20 @@
+import './FlipperCard.css'
+
+const FlipperCard = () => {
+return ( 
+    <>
+      <div class="card">
+        <div class="card-inner">
+          <div class="card-front">
+            <p>Front Side</p>
+          </div>
+          <div class="card-back">
+            <p>Back Side</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default FlipperCard;
